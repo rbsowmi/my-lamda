cdcffa5ab650ee2a8a0b673265e1738d7719e69a
nandha <nandhaaas@gmail.com>
2024-12-18 19:14:06 -0600

    added build spec

---
+++ upload_to_lambda.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Lambda files got changed. Going to update lambda function code"
+aws ssm get-parameters-by-path --path /myLamda --region ap-south-1 | jq -r '.Parameters | map(.Name+"="+.Value)| join("\n") | sub("/myLamda/"; ""; "g")  ' >.envmkdir -p /tmp/myLamda
+mv dist/index.zip /tmp/myLamda/index.zip
+aws s3 cp /tmp/myLamda/Test.zip s3://projectscicds/index.zip
+aws lambda update-function-code --function-name myLamda --s3-bucket projectscicds --s3-key projectscicds/index.zip
