1bb044043c68a12b4e2e00c85e10e6c2cb52ef0b
nandha <nandhaaas@gmail.com>
2024-12-18 19:42:13 -0600

    updated script

--- upload_to_lambda.sh
+++ upload_to_lambda.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "Lambda files got changed. Going to update lambda function code"
-aws ssm get-parameters-by-path --path /myLamda --region ap-south-1 | jq -r '.Parameters | map(.Name+"="+.Value)| join("\n") | sub("/myLamda/"; ""; "g")  ' >.env mkdir -p /tmp/myLamda
+aws ssm get-parameters-by-path --path /myLamda --region ap-south-1 | jq -r '.Parameters | map(.Name+"="+.Value)| join("\n") | sub("/myLamda/"; ""; "g")  ' >.env 
+mkdir -p /tmp/myLamda
 mv dist/index.zip /tmp/myLamda/index.zip
 cp .env /tmp/myLamda/.env
 aws s3 cp /tmp/myLamda/Test.zip s3://projectscicds/index.zip
